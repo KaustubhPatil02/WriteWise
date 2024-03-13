3247165f365a4cf810d61e4da33b71320309af56
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-03-13 22:56:38 +0530

    Add user profile navigation and modal functionality

---
+++ src/components/Home/UserProfile/EditProfileModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Modal from '../../../utility/Modal'
+
+const EditProfileModal = ({editModal, setEditModal}) => {
+  return (
+    <Modal modal={editModal} setModal={setEditModal}>
+       <div>
+        {/* edit functionality for users */}
+       </div>
+    </Modal>
+  )
+}
+
+export default EditProfileModal
---
+++ src/components/Home/UserProfile/UserNavs/UserProfileAbout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserProfileAbout = () => {
+  return (
+    <div>UserProfileAbout</div>
+  )
+}
+
+export default UserProfileAbout
