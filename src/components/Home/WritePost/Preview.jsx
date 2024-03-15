4d4c4f6bd313f47dbb156e45bb3c3685f4ae0ddc
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-03-16 02:32:53 +0530

    Refactor Preview component to add image upload functionality and integrate ReactQuill

--- src/components/Home/WritePost/Preview.jsx
+++ src/components/Home/WritePost/Preview.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import { IoIosCloseCircle  } from "react-icons/io";
+import React, { useRef, useState } from 'react'
+import { IoIosCloseCircle } from "react-icons/io";
+import ReactQuill from 'react-quill';
+
 
 const Preview = () => {
+  const [content, setContent] = useState('');
+
+  const imgRef = useRef(null);
+  const [imgPrev, setImgPrev] = useState("");
+  const clickHandler = () => {
+    imgRef.current.click();
+  }
   return (
     <section className='absolute inset-0 bg-white z-30'>
-        <div className='size my-[2rem]'>
-            <span className='absolute right-[1rem] md:right-[5rem] top-[3rem] text-2xl cursor-pointer'>
-             <IoIosCloseCircle />   
-            </span>
-            <div className='mt-[8rem] flex flex-col md:flex-row gap-10'>
-                <div className='flex-1'>
-                    <h3>Post Preview</h3>
-                    <div className='w-full h-[200px] object-cover bg-gray-100 my-3 grid place-items-center
-                            cursor-pointer bg-cover bg-no-repeat'>Add an Image Cover</div>
-                            <input type="file" hidden
-                             />
-                </div>
-                <div className='flex-1'>rigtside</div>
-            </div>
+      <div className='size my-[2rem]'>
+        <span className='absolute right-[1rem] md:right-[5rem] top-[3rem] text-2xl cursor-pointer'>
+          <IoIosCloseCircle />
+        </span>
+        <div className='mt-[8rem] flex flex-col md:flex-row gap-10'>
+          <div className='flex-1'>
+            <h3>Post Preview</h3>
+            <div
+              style={{backgroundImage: `url(${imgPrev})`}}
+              onClick={clickHandler}
+              className='w-full h-[200px] object-cover bg-gray-100 my-3 grid place-items-center
+                            cursor-pointer bg-cover bg-no-repeat'>
+                              {!imgPrev && <p className='text-2xl text-gray-500'>Add an banner for blog</p>}
+                            </div>
+            <input 
+              onChange={(e) => setImgPrev(URL.createObjectURL(e.target.files[0]))}
+              ref={imgRef} 
+              type="file" 
+              hidden
+            />
+            <input className=' outline-none w-full border-b border-gray-400 py-4' 
+            type="text" 
+            placeholder="Title" 
+        />
+        {/* <ReactQuill theme="snow" value={value} onChange={setValue} placeholder='Content here!' /> */}
+        <ReactQuill 
+        className='border-b border-gray-400 py-5'
+        theme="bubble" 
+        // value={content} onChange={setContent} 
+        placeholder='Content here!' />
+          </div>
+          <div className='flex-1 flex-col gap-[4] mb-5 md:mb-0'>
+            <h3 className='text-2xl '>Publishing to: <span className='font-bold capitalize'>UserName</span></h3>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
