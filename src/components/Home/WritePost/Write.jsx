98824345006f9ffcde5e99351285298e9f2a9253
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-03-22 22:53:14 +0530

    posting of first blog complete- update in firabase

--- src/components/Home/WritePost/Write.jsx
+++ src/components/Home/WritePost/Write.jsx
@@ -1,30 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import Preview from './Preview';
 import { Blog } from '../../../Context/Context';
-// import 'react-quill/dist/quill.snow.css'; // for snow theme
 
 const Write = () => {
-    const [content, setContent] = useState('');
-    const { publish, setPublish } = Blog();
-    return (
-        <section className='w-[90%] md:w-[80%] lg:w-[60%] mx-auto py-[3rem]'>
+  const { publish, setPublish } = Blog();
+  const [ideas, setIdeas] = useState('');
+  const [title, setTitle] = useState('');
 
-            <input className='text-5xl outline-none w-full'
-                type="text"
-                placeholder="Title"
-            />
-            {/* <ReactQuill theme="snow" value={value} onChange={setValue} placeholder='Content here!' /> */}
-            <ReactQuill
-                className='write my-5'
-                theme="bubble" value={content} onChange={setContent} placeholder='Content here!' 
-            />
-            <div 
-            className={`${publish ? "visible opacity-100" :"invisible opacity-0"} transition-all duration-200`}>
-                <Preview setPublish={setPublish} />
-            </div>
-        </section>
-    )
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    // Handle form submission logic here
+  };
+
+  return (
+    <section className='w-[90%] md:w-[80%] lg:w-[60%] mx-auto py-[3rem]'>
+      <form onSubmit={handleSubmit}>
+        <input
+          className='text-5xl outline-none w-full'
+          value={title}
+          type="text"
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <ReactQuill
+          value={ideas}
+          onChange={setIdeas}
+          className='write my-5'
+          theme="bubble"
+          placeholder='Push your Content or Ideas here!'
+        />
+        <div className={`${publish ? "visible opacity-100" : "invisible opacity-0"} transition-all duration-200`}>
+          <Preview
+            setPublish={setPublish}
+            title={title}
+            ideas={ideas}
+          />
+        </div>
+        <button type="submit" style={{ display: 'none' }}></button> {/* Hidden submit button */}
+      </form>
+    </section>
+  );
 }
 
-export default Write
+export default Write;
