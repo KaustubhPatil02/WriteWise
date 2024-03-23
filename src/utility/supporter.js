e89799c6d1283acafaad35dd8d49417fa67dcb66
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-03-23 17:18:53 +0530

    Add readTime function to calculate estimated reading time

--- src/utility/supporter.js
+++ src/utility/supporter.js
@@ -8,3 +8,14 @@ export const secretMail = (email) =>{
     // console.log(email)
     return `${secret}`
 }
+
+export const readTime = (desc) =>{
+    const avgReadTime = 200;
+
+    const div = document.createElement('div');
+    div.innerHTML = desc.__html;
+
+    const textContext = div.textContent || div.innerHTML;
+    const words = textContext.trim().split(/\s+/).length;
+    return Math.ceil(words.length / avgReadTime);
+}
